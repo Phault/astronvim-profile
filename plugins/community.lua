66d25358de3992bd32418831b8bd96af9abc3a7b
Casper Lindschouw <casper@lindschouw.me>
2023-09-02 12:11:14 +0200

    add blame.nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -20,4 +20,5 @@ return {
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.astro" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.git.blame-nvim" },
 }
