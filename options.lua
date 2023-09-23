269effaaee95eb2b704cb2482ed139b6c891cf99
Casper Lindschouw <casper@lindschouw.me>
2023-09-23 20:11:42 +0200

    some cleanup

--- options.lua
+++ options.lua
@@ -1,7 +1,6 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
 return {
   opt = {
-    -- set to true or false etc.
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
@@ -19,12 +18,3 @@ return {
     resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
   },
 }
--- If you need more control, you can use the function()...end notation
--- return function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,11 +1,5 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.cs" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.bash" },
@@ -15,10 +9,11 @@ return {
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.terraform" },
+  -- { import = "astrocommunity.pack.terraform" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.git.blame-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
